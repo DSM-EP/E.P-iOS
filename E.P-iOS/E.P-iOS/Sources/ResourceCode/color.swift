461a0eda53d6381f8c1117ada0d837c35f0ed56e
Kim <xodnjs0876@gmail.com>
2023-04-17 16:19:28 +0900

    ⚙️ :: Color

---
+++ E.P-iOS/E.P-iOS/Sources/ResourceCode/color.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension UIColor {
+    static let mainColor = UIColor(named: "MainColor")!
+    static let subColor = UIColor(named: "SubColor")!
+    static let red = UIColor(named: "Red")!
+    static let orange = UIColor(named: "Orange")!
+    static let yellow = UIColor(named: "Yellow")!
+    static let green = UIColor(named: "Green")!
+    static let Brown = UIColor(named: "Brown")!
+}
