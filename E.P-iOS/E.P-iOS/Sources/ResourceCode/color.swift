3da55e4f8db21361fc623ee79b8e4ab8208cbafa
Kim <xodnjs0876@gmail.com>
2023-04-21 10:18:00 +0900

    ⚡️:: Color 추가 및 수정

--- E.P-iOS/E.P-iOS/Sources/ResourceCode/color.swift
+++ E.P-iOS/E.P-iOS/Sources/ResourceCode/color.swift
@@ -1,11 +1,7 @@
 import UIKit
 
 extension UIColor {
-    static let mainColor = UIColor(named: "MainColor")!
-    static let subColor = UIColor(named: "SubColor")!
     static let red = UIColor(named: "Red")!
-    static let orange = UIColor(named: "Orange")!
-    static let yellow = UIColor(named: "Yellow")!
     static let green = UIColor(named: "Green")!
-    static let Brown = UIColor(named: "Brown")!
+    static let green1 = UIColor(named: "Green-1")
 }
