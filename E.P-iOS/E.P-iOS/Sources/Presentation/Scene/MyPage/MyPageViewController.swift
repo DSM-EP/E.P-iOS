4e870a6893f45d8c0094ce6def7c21ac1f2118b4
Kim <xodnjs0876@gmail.com>
2023-04-25 22:28:03 +0900

    ⚡️:: 마이페이지 퍼블리싱 수정

--- E.P-iOS/E.P-iOS/Sources/Presentation/Scene/MyPage/MyPageViewController.swift
+++ E.P-iOS/E.P-iOS/Sources/Presentation/Scene/MyPage/MyPageViewController.swift
@@ -1,29 +1,79 @@
-//
-//  MyPageViewController.swift
-//  E.P-iOS
-//
-//  Created by 김태원 on 2023/04/24.
-//
-
 import UIKit
+
+import SnapKit
+import Then
 
 class MyPageViewController: UIViewController {
 
+    // MARK: - UI
+    private let profileImage = UIImageView().then {
+        $0.contentMode = .scaleToFill
+        $0.image = UIImage(named: "Profile")
+    }
+    private let userName = UILabel().then {
+        $0.font = .notoSansFont(ofSize: 17, family: .medium)
+        $0.text = "오상우"
+    }
+    private let styleTitle = UILabel().then {
+        $0.font = .notoSansFont(ofSize: 15, family: .regular)
+        $0.text = "홀리몰리"
+    }
+    private let contentView = UIView().then {
+        $0.backgroundColor = .green1
+    }
+    private let noticeLabel = UILabel().then {
+        $0.text = "우리가 사는 지구\n언제까지 죽일건가요\n더 이상 죽이지 말고 살립시다\n절약을 하며\n여러분이 살 세상을 만드세요"
+        $0.numberOfLines = 0
+        $0.textAlignment = .center
+        $0.font = .notoSansFont(ofSize: 26, family: .medium)
+        $0.textColor = .white
+    }
+
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
-    */
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        addSubviews()
+        makeSubviewConstraints()
+    }
+}
 
+// MARK: - Layout
+extension MyPageViewController {
+    func addSubviews() {
+        [profileImage, userName, styleTitle, noticeLabel, contentView].forEach { view.addSubview($0)}
+        
+        contentView.addSubview(noticeLabel)
+    }
+    func makeSubviewConstraints() {
+        profileImage.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(63)
+            $0.left.equalToSuperview().inset(47)
+            $0.width.height.equalTo(85)
+        }
+        userName.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(98)
+            $0.leading.equalTo(profileImage.snp.trailing).offset(16)
+        }
+        styleTitle.snp.makeConstraints {
+            $0.top.equalTo(userName.snp.bottom)
+            $0.leading.equalTo(profileImage.snp.trailing).offset(16)
+        }
+        contentView.snp.makeConstraints {
+            $0.top.equalTo(styleTitle.snp.bottom).offset(25)
+            $0.centerX.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(18)
+            $0.height.equalTo(626)
+        }
+        noticeLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(157)
+            $0.centerX.equalToSuperview()
+        }
+    }
 }
